5f2630c5a56979058f7d20d0541dd37f45191142
n1a2v3n4e5e6l7 <64906699+n1a2v3n4e5e6l7@users.noreply.github.com>
2020-08-05 17:38:45 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,6 @@
+class Form{
+constructor(){
+    this.button=createButton('Start');
+    this.title = createElement('h1');
+}    
+}
---
+++ sketch.js
@@ -0,0 +1,10 @@
+var tasks= ["read a magazine","stare outside the window","write a story","create a card for how thankful you are to your mother","Spend time with your friend/sibling"];
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
